05ccc13fabf2c3afd93ed8c15f82b460c4d6691d
Ansif M Shamsu <ansifofficialacc@gmail.com>
2024-03-08 19:40:51 +0530

    Optimized Double Thumb Slider Implementation

--- milesmart-ui/src/app/components/range_slider.tsx
+++ milesmart-ui/src/app/components/range_slider.tsx
@@ -1,8 +1,8 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
-export default function RangeSlider({min, max, onChange}: {min:number, max:number, onChange: any}) {
-    const [minVal, setMinVal] = useState(min);
-    const [maxVal, setMaxVal] = useState(max);
+export default function RangeSlider({min = 0, max = 100, value_min = 0, value_max = 100, onChange}: {min?:number, max?:number, value_min?:number, value_max?:number, onChange?: any}) {
+    // const [minVal, setMinVal] = useState(min);
+    // const [maxVal, setMaxVal] = useState(max);
 
     const minValRef = useRef<HTMLInputElement>(null);
     const maxValRef = useRef<HTMLInputElement>(null);
@@ -14,34 +14,46 @@ export default function RangeSlider({min, max, onChange}: {min:number, max:numbe
     );
 
     // Set width of the range to decrease from the left side
-    useEffect(() => {
-    if (maxValRef.current) {
-        const minPercent = getPercent(minVal);
-        const maxPercent = getPercent(+maxValRef.current.value); 
+    // useEffect(() => {
+    // if (maxValRef.current) {
+    //     const minPercent = getPercent(value_min);
+    //     const maxPercent = getPercent(+maxValRef.current.value); 
 
-        if (range.current) {
-        range.current.style.left = `${minPercent}%`;
-        range.current.style.width = `${maxPercent - minPercent}%`;
-        }
-    }
-    }, [minVal, getPercent]);
+    //     if (range.current) {
+    //     range.current.style.left = `${minPercent}%`;
+    //     range.current.style.width = `${maxPercent - minPercent}%`;
+    //     }
+    // }
+    // }, [value_min, getPercent]);
 
-    // Set width of the range to decrease from the right side
-    useEffect(() => {
-    if (minValRef.current) {
-        const minPercent = getPercent(+minValRef.current.value);
-        const maxPercent = getPercent(maxVal);
+    // // Set width of the range to decrease from the right side
+    // useEffect(() => {
+    // if (minValRef.current) {
+    //     const minPercent = getPercent(+minValRef.current.value);
+    //     const maxPercent = getPercent(value_max);
 
-        if (range.current) {
-        range.current.style.width = `${maxPercent - minPercent}%`;
-        }
-    }
-    }, [maxVal, getPercent]);
+    //     if (range.current) {
+    //     range.current.style.width = `${maxPercent - minPercent}%`;
+    //     }
+    // }
+    // }, [value_max, getPercent]);
 
     // Get min and max values when their state changes
+    // useEffect(() => {
+    //     onChange({ min: value_min, max: value_max });
+    // }, [value_min, value_max, onChange]);
+
     useEffect(() => {
-        onChange({ min: minVal, max: maxVal });
-    }, [minVal, maxVal, onChange]);
+        if (maxValRef.current) {
+            const minPercent = getPercent(value_min);
+            const maxPercent = getPercent(+maxValRef.current.value); 
+
+            if (range.current) {
+                range.current.style.left = `${minPercent}%`;
+                range.current.style.width = `${maxPercent - minPercent}%`;
+            }
+        }
+    }, [min, max, value_min, value_max])
 
     return (
         <div>
@@ -49,11 +61,12 @@ export default function RangeSlider({min, max, onChange}: {min:number, max:numbe
                 type="range"
                 min={min}
                 max={max}
-                value={minVal}
+                value={value_min}
                 ref={minValRef}
                 onChange={(event) => {
-                    const value = Math.min(+event.target.value, maxVal - 1);
-                    setMinVal(value);
+                    const value = Math.min(+event.target.value, value_max - 1);
+                    onChange({ min: value, max: value_max })
+                    // setMinVal(value);
                     event.target.value = value.toString();
                 }}
                 className="thumb thumb--zindex-3 [&::-webkit-slider-thumb]:bg-black [&::-moz-range-thumb]:bg-black dark:[&::-webkit-slider-thumb]:bg-gray-500 dark:[&::-moz-range-thumb]:bg-gray-500"
@@ -62,11 +75,12 @@ export default function RangeSlider({min, max, onChange}: {min:number, max:numbe
                 type="range"
                 min={min}
                 max={max}
-                value={maxVal}
+                value={value_max}
                 ref={maxValRef}
                 onChange={(event) => {
-                    const value = Math.max(+event.target.value, minVal + 1);
-                    setMaxVal(value);
+                    const value = Math.max(+event.target.value, value_min + 1);
+                    onChange({ min: value_min, max: value })
+                    // setMaxVal(value);
                     event.target.value = value.toString();
                 }}
                 className="thumb thumb--zindex-3 [&::-webkit-slider-thumb]:bg-black [&::-moz-range-thumb]:bg-black dark:[&::-webkit-slider-thumb]:bg-gray-500 dark:[&::-moz-range-thumb]:bg-gray-500"
--- milesmart-ui/src/app/results/page.tsx
+++ milesmart-ui/src/app/results/page.tsx
@@ -5,14 +5,24 @@ import './styles.css'
 import RangeSlider from '../components/range_slider';
 
 export default function Results() {
+    const rangeSlider = useRef<HTMLInputElement>(null)
+    const [rangeSliderValue, setRangeSliderValue] = useState(50)
+    const [minVal, setMinVal] = useState(25)
+    const [maxVal, setMaxVal] = useState(75)
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-gray-900">
             <div className='m-auto'>
-                <RangeSlider min={0} max={100} onChange={
+                <RangeSlider min={0} max={100} value_min={minVal} value_max={maxVal} onChange={
                     ({min, max}: {min:number, max:number}) => {
                         console.log(`min = ${min}, max = ${max}`)
+                        setMinVal(min)
+                        setMaxVal(max)
                     }
                 }/>
+                <input ref={rangeSlider} type='range' className='m-8' value={rangeSliderValue} onChange={(e) => {
+                    if (rangeSlider.current != null) setRangeSliderValue(parseInt(rangeSlider.current.value))
+                    else console.log('Error')
+                }} />
             </div>
         </main>
     )
