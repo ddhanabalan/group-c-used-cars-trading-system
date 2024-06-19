bb6ac1e516aff0d070d300a466cabf08ebc08f44
Sanchu M <sanchum1363@gmail.com>
2024-06-19 18:31:28 +0530

    Bug fixes on header bar impl

--- milesmart-ui/src/app/admin/page.tsx
+++ milesmart-ui/src/app/admin/page.tsx
@@ -1,14 +1,18 @@
+import HeaderBar from "../components/header_bar";
 import Coadmin from "./components/Coadmin";
 import Cosidebar from "./components/Cosidebar";
 
 export default async function Home() {
     return (
-        <main className="flex flex-row gap-4 h-screen p-4 bg-[#F5F5F5]">
-        <Cosidebar/>
-        <div className="mt-16 bg-white rounded-3xl basis-3/4">
-          
-          <Coadmin />
-        </div>
+        <main className="flex flex-col min-h-screen bg-[#F5F5F5]">
+          <HeaderBar />
+          <div className="flex flex-row gap-4 p-4 ">
+            <Cosidebar/>
+            <div className="mt-16 bg-white rounded-3xl basis-3/4">  
+              <Coadmin />
+            </div>
+          </div>
+        
       </main>
     );
   }
